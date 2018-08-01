eec25c7fbd99bc6cb736337adddcfdef1df73dad
石博 <shibo@shibodeMacBook-Air.local>
2018-08-01 10:23:10 +0800

    Initial commit

---
+++ test/src/main/java/com/dxxx/test/BaseBean.java
@@ -0,0 +1,8 @@
+package com.dxxx.test;
+
+/**
+ * Created by shibo on 2018/7/30.
+ */
+
+public class BaseBean {
+}
---
+++ test/src/main/java/com/dxxx/test/MainActivity.java
@@ -0,0 +1,89 @@
+package com.dxxx.test;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.dxxx.refreshloadmorerecycleview.recycleview.RefreshRecycleview;
+import com.dxxx.refreshloadmorerecycleview.recycleview.Refresh_Loadmore_Layout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    Handler handler;
+    List<BaseBean> all;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        handler = new Handler();
+        // heard.setPadding(0,170,0,0);
+        final Refresh_Loadmore_Layout refresh_loadmore_layout = (Refresh_Loadmore_Layout) findViewById(R.id.refresh);
+        all = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            all.add(new BaseBean());
+        }
+        final SimpleRecycleviewAdater simpleRecycleviewAdater = new SimpleRecycleviewAdater(all, MainActivity.this);
+        refresh_loadmore_layout.setAdapter(simpleRecycleviewAdater);
+        refresh_loadmore_layout.setRefreshTouchEvent(new RefreshRecycleview.RefreshTouchEvent() {
+            @Override
+            public void onRefrshStart() {
+                Log.e("refreshRecycleview", "onRefrshStart");
+            }
+
+            @Override
+            public void onRefreshing() {
+                Log.e("refreshRecycleview", "onRefreshing");
+            }
+
+            @Override
+            public void onRefresh() {
+
+                Log.e("refreshRecycleview", "onRefresh");
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.e("发送关闭", "===");
+                        refresh_loadmore_layout.stopRefreshOrLoadmore();
+                        all.remove(0);
+                        simpleRecycleviewAdater.notifyDataSetChanged();
+                    }
+                }, 2000);
+
+            }
+
+            @Override
+            public void onLoadmoreStart() {
+
+                Log.e("refreshRecycleview", "onLoadmoreStart");
+
+            }
+
+            @Override
+            public void onLoading() {
+                Log.e("refreshRecycleview", "onLoading");
+
+            }
+
+            @Override
+            public void onLoadmore() {
+                Log.e("refreshRecycleview", "onLoadmore");
+
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        refresh_loadmore_layout.stopRefreshOrLoadmore();
+                        all.add(new BaseBean());
+                        simpleRecycleviewAdater.notifyDataSetChanged();
+                    }
+                }, 2000);
+
+            }
+        });
+
+    }
+}
